8cc9af467475165eeed5a0df4bdcaeb546a37600
Yogesh S <yogeshs.ai2023@dce.edu.in>
2026-07-09 14:10:19 +0530

    Time: 3 ms (45.92%), Space: 23.3 MB (97.24%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,12 +5,7 @@ class Solution:
         """
         left = 0
         right = len(s) - 1
-        
-        
         while left < right:
-            
             s[left], s[right] = s[right], s[left]
-            
-            
             left += 1
             right -= 1   
